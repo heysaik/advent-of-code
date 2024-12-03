17c9c5a0f81c1ec6c8c90d66d8a28444bebfaa1c
Sai Kambampati <sai@kambampati.studio>
2024-12-02 23:22:14 -0800

    Part 1 finished

--- 2024/day-3/src/main.rs
+++ 2024/day-3/src/main.rs
@@ -1,15 +1,24 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use regex::Regex;
 
 fn main() {
     let file_name = "input.txt";
     let file = File::open(&file_name).expect(&format!("Unable to open file: {}", file_name));
     let reader = BufReader::new(file);
+    let mut answer = 0;
 
     for line in reader.lines() {
         match line {
             Ok(text) => {
-                println!("{}", text);
+                // Find all regex matches for "mul(X,Y)" where X and Y are numbers
+                let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+                let matches = regex.find_iter(&text);
+                for result in matches {
+                    let x = result.as_str().split(",").nth(0).unwrap().split("mul(").nth(1).unwrap().parse::<u32>().unwrap();
+                    let y = result.as_str().split(",").nth(1).unwrap().split(")").nth(0).unwrap().parse::<u32>().unwrap();
+                    answer += (x * y);
+                }
             }
 
             Err(e) => {
@@ -17,4 +26,6 @@ fn main() {
             }
         }
     }
+
+    println!("The result adding all the multiplication values is: {answer}");
 }
